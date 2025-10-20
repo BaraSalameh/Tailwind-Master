6096579650c01b0083ec9acaba4e59a986279736
Bara E. Salameh <barasalameh90@gmail.com>
2025-10-20 03:41:15 +0300

    -Instant text area focus on load (random choice picker challenge)

--- src/app/challenges/random-choice-picker/page.tsx
+++ src/app/challenges/random-choice-picker/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { KeyboardEvent, useRef, useState } from "react";
+import { KeyboardEvent, useEffect, useRef, useState } from "react";
 
 const RandomChoicePicker = () => {
     const choicesRef = useRef<HTMLParagraphElement[]>([]);
@@ -37,15 +37,23 @@ const RandomChoicePicker = () => {
         }, 5000);
     }
 
+    useEffect(() => {
+        inputRef.current?.focus();
+    }, [])
+
     return (
         <div className="bg-[#2b88f0] flex items-center justify-center h-screen text-foreground">
-            <div className="flex flex-col w-2/3 gap-5">
+            <div className="flex flex-col w-2/3 sm:w-1/2">
                 <h1 className="text-xl text-center">
                     Enter all of the choices divided by a comma (','). Press enter when you're done
                 </h1>
                 <textarea
                     ref={inputRef}
-                    className="bg-foreground placeholder:text-background placeholder:opacity-60 p-2 w-full text-background"
+                    className="
+                        bg-foreground text-background focus:outline-0 rounded-lg
+                        placeholder:text-background placeholder:opacity-60
+                        p-2 w-full
+                    "
                     placeholder="Enter choices here..."
                     rows={4}
                     onChange={e => setChoices(e.target.value)}
