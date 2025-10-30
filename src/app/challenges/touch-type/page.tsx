e724a14cee10fe25adf21ad18b76edcfc761bbff
Bara E. Salameh <barasalameh90@gmail.com>
2025-10-30 04:04:10 +0200

    touch type challenge v2

--- src/app/challenges/touch-type/page.tsx
+++ src/app/challenges/touch-type/page.tsx
@@ -1,53 +1,41 @@
 'use client';
 
-import { X } from "lucide-react";
+import { Redo2, X } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
 const TouchType = () => {
     const [isDone, setIsDone] = useState(false);
-    const [paragraph, setParagraph] = useState<string[]>([]);
-    const [userInput, setUserInput] = useState<string[]>(['']);
-    const [result, setResult] = useState({});
+    const [paragraph, setParagraph] = useState('');
+    const [userInput, setUserInput] = useState('');
     const inputRef = useRef<HTMLInputElement>(null);
-    
+
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        
         if(userInput.length < 1) return;
         if(inputRef.current) inputRef.current.disabled = true;
-
         setIsDone(true);
-        setResult(
-            Object.fromEntries(
-                Array.from({length: userInput.length}).map((_, idx) => 
-                    [paragraph[idx], paragraph[idx] === userInput[idx]]
-                )
-            )
-        )
     }
 
     const handleClose = () => {
         setIsDone(false);
-        setUserInput(['']);
-        setResult({});
+        setUserInput('');
         if(inputRef.current) {
             inputRef.current.disabled = false;
             inputRef.current.focus();
         }
     }
 
+    
     useEffect(() => {
         const retrieveParagraph = async () => {
             await fetch('https://api.api-ninjas.com/v1/loremipsum?paragraphs=1&start_with_lorem_ipsum=false', {
                 headers: {'X-Api-Key': '4fe5mUnMeRElkxN7kxuvMQ==H0PkLgd3pZhYv0hw'}
             })
             .then(res => res.json())
-            .then(data => {
-                setParagraph(data.text.trimEnd().split(" "))}
-            )
-            .catch(() => setParagraph([]));
+            .then(data => setParagraph(data.text.trimEnd()))
+            .catch(() => setParagraph(''));
         }
-
+        
         retrieveParagraph();
         inputRef.current?.focus();
     }, []);
@@ -65,26 +53,27 @@ const TouchType = () => {
                 <p
                     className="text-nowrap "
                     style={{
-                        transform: `translateX(calc(30% - ${userInput.join(" ").length}ch))`
+                        transform: `translateX(calc(30% - ${userInput.length}ch))`
                     }}
                 >
-                    {paragraph.map((word, idx) =>
+                    {paragraph.split('').map((char, idx) =>
                         <span
                             key={idx}
                             className={`
-                                inline-block
-                                ${idx % 2 !== 0 && 'mx-[1ch]'}
-                                ${idx >= userInput.length
-                                    ? 'text-gray-700'
-                                    : userInput.length - 1 > idx
-                                        ? userInput[idx] === word
-                                            ? 'text-green-700'
+                                whitespace-pre
+                                ${userInput.length <= idx
+                                    ? userInput.length === idx
+                                        ? 'text-foreground'
+                                        : 'text-gray-700'
+                                    : userInput[idx] === char
+                                        ? 'text-green-700'
+                                        : char === ' '
+                                            ? 'bg-red-700 scale-80 translate-x-1/10'
                                             : 'text-red-700'
-                                        : ''
                                 }
                             `}
                         >
-                            {word}
+                            {char}
                         </span>
                     )}
                 </p>
@@ -96,8 +85,8 @@ const TouchType = () => {
                         onPaste={e => e.preventDefault()}
                         ref={inputRef}
                         type="text"
-                        onChange={e => setUserInput(e.target.value.split(" "))}
-                        value={userInput.join(" ")}
+                        onChange={e => setUserInput(e.target.value)}
+                        value={userInput}
                     />
                 </form>
             </div>
@@ -114,12 +103,18 @@ const TouchType = () => {
                         onClick={handleClose}
                     />
                     <p className="flex flex-wrap text-lg">
-                        {Object.entries(result).map(([key, value], idx) =>
-                            <span
+                        {userInput.split('').map((char, idx) => 
+                             <span
                                 key={idx}
-                                className={`m-[0.5ch] ${value ? 'text-green-700' : 'text-red-700'}`}
+                                className={`
+                                    whitespace-pre
+                                    ${char === paragraph[idx]
+                                        ?   'text-green-700'
+                                        :   'text-red-700'
+                                    }    
+                                `}
                             >
-                                {key}
+                                {char}
                             </span>
                         )}
                     </p>
